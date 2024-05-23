0e5bdea470219bea161d29f5ea8d239f01f0da80
nadyb <sad@sda.ru>
2024-05-23 21:25:02 +0300

    new add

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-
+user = get_user_model()
 class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
+    author = models.ForeignKey(user,models.CASCADE)
 
     def __str__(self):
         return 'Post'+self.title
@@ -12,3 +14,4 @@ class Post(models.Model):
 class Comment(models.Model):
     text = models.TextField()
     to_post = models.ForeignKey(Post, models.CASCADE)
+
--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,67 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from blog.models import Post, Comment
+from blog.templates.forms import PostForm, CommentForm
 
 
 def main_view(request):
     posts=Post.objects.all()
     for i in posts:
         print(i.title)
-    return render(
-        request,
-        'main_page.html',
-        {'pst':posts},
 
-    )
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            data = request.POST
+            print(data)
+            form = PostForm(data)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.save()
+                post_id = post.id
+                return redirect(f"/post/{post_id}/")
+        else:
+            form = PostForm()
+        return render(
+            request,
+            'main_page.html',
+            {'pst': posts,
+             'form': form,
+             'user': request.user,},
+
+        )
+    else:
+        return render(
+            request,
+            'main_page.html',
+            {'pst': posts,
+                    'user': request.user,})
+
 
 def post_view(request,pk):
     qs = Post.objects.filter(pk=pk)
     post = qs.first()
     comments = Comment.objects.filter(to_post=post)
     comments = comments.all()
+    form = CommentForm()
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            data = request.POST
+            print(data)
+            form = CommentForm(data)
+            if form.is_valid():
+                comment = form.save(commit=False)
+                # post.author = request.user
+                comment.to_post = post
+                comment.save()
+
     return render(
         request,
         'one_post.html',
         {'post': post,
-                'comments' : comments},
+                'comments' : comments,
+                'form':form,
+                'user':request.user,
+                }
     )
